c41f8ab26edad153a1e16d55179e3337919a1635
waka <p.c.spruijtenburg@utwente.nl>
2013-12-26 12:09:01 +0100

    -abstractions and simplifications for parspace -hdf5 output added -hilbert now takes more than 2 dimensions

--- measure_scripts/testParspace.py
+++ measure_scripts/testParspace.py
@@ -4,6 +4,7 @@
 
 dsgen1 = qt.instruments.create('dsgen1', 'dummy_signal_generator')
 dsgen2 = qt.instruments.create('dsgen2', 'dummy_signal_generator')
+dsgen3 = qt.instruments.create('dsgen3', 'dummy_signal_generator')
 
 import lib.parspace as ps
 reload(ps)
@@ -13,33 +14,51 @@ ax1 = ps.param()
 ax1.begin = 2.
 ax1.end = 10.
 ax1.stepsize = 1.
-ax1.rate_stepsize = .5
-ax1.rate_delay = 55
+ax1.rate_stepsize = 1.
+ax1.rate_delay = 10
 ax1.instrument = 'dsgen1'
-ax1.instrument_opt = 'amplitude'
-ax1.module = lambda x: dsgen1.set_amplitude(x)
+ax1.label = 'x'
+ax1.module_name = 's1f'#'dac','s1c'
+ax1.module_options = {'dac':5, 
+						'rate_stepsize':.5,
+						'rate_delay': 10.,
+						'var':'amplitude',
+						'amplification':'100M' }
 
 
 ax2 = ps.param()
 ax2.begin = 5.
-ax2.end = 10.
+ax2.end = 30.
 ax2.stepsize = 1.
+ax2.label = 'y'
 ax2.rate_stepsize = .5
-ax2.rate_delay = 55
+ax2.rate_delay = 10
 ax2.instrument = 'dsgen2'
-ax2.instrument_opt = 'amplitude'
+ax2.module_name = 's1f'#'dac','s1c'
+ax2.module_options = {'dac':5, 
+						'rate_stepsize':.5,
+						'rate_delay': 10.,
+						'var':'amplitude',
+						'amplification':'100M' }
 ax2.module = lambda x: dsgen2.set_amplitude(x)
 
+import copy
+ax3 = copy.deepcopy(ax2)
+ax3.label = 'z'
+ax3.instrument='dsgen3'
+
 z = ps.param()
-z.module = lambda: dsgen1.get_amplitude() + dsgen2.get_amplitude()
+z.label = 'value'
+z.module = lambda: dsgen1.get_amplitude() + dsgen2.get_amplitude() + dsgen3.get_amplitude()
 
 
 ping = ps.parspace()
 ping.add_param(ax1)
 ping.add_param(ax2)
-# ping.add_param(ax2)
+ping.add_param(ax3)
+
 ping.add_paramz(z)
 
 ping.set_traversefunc(lambda axes,**lopts: ps.sweep_func_helper(axes,datablock='on',**lopts))
-ping.set_traversefuncbyname('sweep',n=3,sweepback='off')
+ping.set_traversefuncbyname('hilbert',n=5,sweepback='off')
 ping.traverse()
--- source/lib/parspace.py
+++ source/lib/parspace.py
@@ -7,9 +7,9 @@ import numpy as np
 from time import time,sleep
 import os
 import qt
+import hdf5_data as h5
 
 curve = np.zeros((0,2))
-# curve = np.mat(curve.copy())
 
 #generator version of hilbert space function
 def hilbert_it(x0, y0, xi, xj, yi, yj, n):
@@ -18,7 +18,8 @@ def hilbert_it(x0, y0, xi, xj, yi, yj, n):
         Y = y0 + (xj + yj)/2
         
         # Output the coordinates of the cv
-        yield {'dp':((X,Y))}
+        yield {'dp':[X,Y]}
+
     else:
         for i in hilbert_it(x0,               y0,               yi/2, yj/2, xi/2, xj/2, n - 1):
             yield i
@@ -29,29 +30,17 @@ def hilbert_it(x0, y0, xi, xj, yi, yj, n):
         for i in hilbert_it(x0 + xi/2 + yi,   y0 + xj/2 + yj,  -yi/2,-yj/2,-xi/2,-xj/2, n - 1):
             yield i
 
-def hilbert(x0, y0, xi, xj, yi, yj, n):
-    if n <= 0:
-        X = x0 + (xi + yi)/2
-        Y = y0 + (xj + yj)/2
-        
-        # Output the coordinates of the cv
-        global curve
-        #curve=np.append(curve, [[X,Y]],axis=0)
-        curve=np.vstack((curve,(X,Y)))
-        #print '%s %s 0' % (X, Y)
-    else:
-        hilbert(x0,               y0,               yi/2, yj/2, xi/2, xj/2, n - 1)
-        hilbert(x0 + xi/2,        y0 + xj/2,        xi/2, xj/2, yi/2, yj/2, n - 1)
-        hilbert(x0 + xi/2 + yi/2, y0 + xj/2 + yj/2, xi/2, xj/2, yi/2, yj/2, n - 1)
-        hilbert(x0 + xi/2 + yi,   y0 + xj/2 + yj,  -yi/2,-yj/2,-xi/2,-xj/2, n - 1)
-
 def hilbert_gen(xs, n=3,**lopts):
 	return hilbert_it(xs[0].begin, xs[1].begin, xs[0].end-xs[0].begin, 0,0, xs[1].end-xs[1].begin, n)
-
-def param_hilb(xs, n=5,**lopts):
-	hilbert(xs[0].begin, xs[1].begin, xs[0].end-xs[0].begin, 0,0, xs[1].end-xs[1].begin, n)
-	return curve
 	
+def hilbert_mul(xs,n=3,**lopts):
+	if len(xs) > 2:
+		for i in sweep_gen(xs[:-2],**lopts):
+			for j in hilbert_it(xs[-2].begin, xs[-1].begin, xs[-2].end-xs[-2].begin, 0,0, xs[-1].end-xs[-1].begin, n):
+				yield {'dp': np.append(i['dp'],j['dp']), 'newblock':i['newblock']}
+	else:
+		yield hilbert_gen(xs,**lopts)
+
 def sweep_gen(xs,**lopts):
 	#options are sweepback and datablock bit
 	#sets the datablock bit in the part of a dict
@@ -80,48 +69,6 @@ def sweep_gen_helper(xs,**lopts):
 		#add a new datablock bit at the end if the option is set	
 # 		if 'datablock' in lopts and lopts['datablock' == 'on'
 			
-
-def sweep_func_helper(xs, **lopts):
-	z = np.array([])
-	x = xs[0]
-	u = np.arange(x.begin,x.end,x.stepsize)
-	sb_bit = 0
-	if len(xs[1:]) > 0:
-		for uu in u:
-			appendage = sweep_func_helper(xs[1:],**lopts)
-			if 'sweepback' in lopts:
-				if sb_bit:
-					appendage = np.flipud(appendage)
-					if 'datablock' in lopts:
-						#reverse the flip of the datablock bit#hackyhackyTM
-						appendage[:,-1] = np.flipud(appendage[:,-1]) 
-					sb_bit = 0
-				else:
-					sb_bit = 1
-			#if appendage 1d then column stack
-			#if len(appendage.shape) == 1:
-			z_t = np.column_stack((np.repeat(uu,appendage.shape[0]),appendage))
-			if len(z) == 0:
-				z = z_t
-			else:
-				z = np.vstack((z,z_t))
-			# else:
-				# print appendage.shape
-				# z_t = np.concatenate( (np.repeat(uu,appendage.shape[0]),appendage),axis=1)
-				# if len(z) == 0:
-					# z = z_t
-				# else:
-					# z = np.vstack((z,z_t))
-	else:
-		#implement datablock bit
-		if 'datablock' in lopts:
-			z_t = np.zeros(len(u))
-			z_t[-1] = 1
-			z = np.column_stack((u,z_t)) 
-		else:
-			z = u 
-	return z
-
 class param(object):
 	def __init__(self):
 		self.begin = 0
@@ -129,6 +76,8 @@ class param(object):
 		self.instrument = []
 		self.instrument_opt = []
 		self.module = []
+		self.module_name = []
+		self.module_options = {}
 		self.steps = []
 		self.stepsize = []
 		self.rate_stepsize = []
@@ -168,79 +117,117 @@ class parspace(object):
 # 		if isempty(self.xs):
 # 			raise Exception('Define all your axes before choosing a traverse function')
 		if name == 'hilbert':
-			self.traverse_gen = lambda xs: hilbert_gen(xs,*kwargs,**lopts)
+			self.traverse_gen = lambda xs: hilbert_mul(xs,*kwargs,**lopts)
 		elif name == 'sweep':
 			self.traverse_gen = lambda xs: sweep_gen(xs,*kwargs,**lopts)
 		else:
 			raise Exception('Unknown traverse function specified')
-			
-	def data_gen(self):
-		trav = self.lineartraverse
-		cnt = 0
-		while cnt < trav.shape[0]: 
-			yield trav[cnt] #yield a single array row including controls bits
-			cnt += 1
-	
+				
 	def traverse(self):
 		#traverse the defined parameter space, using e.g. a space filling curve defined in self.traverse_func
+		instruments = []
 		for x in self.xs:
 			instr = qt.instruments.get_instruments()[x.instrument]
-			instr.set_parameter_rate(x.instrument_opt,x.rate_stepsize,x.rate_delay)
-		
-		self.lineartraverse = self.traverse_func(self.xs)
+			
+			rate_delay = x.module_options['rate_delay']
+			rate_stepsize = x.module_options['rate_stepsize']
+			variable = x.module_options['var']
+			
+			#transform here also according to chosen module?
+			instr.set_parameter_rate(variable,rate_stepsize,rate_delay)
+			instruments  = np.append(instruments, instr)
+
 		data = qt.Data(name=self.measurementname)
+		dat = h5.HDF5Data(name=self.measurementname)
+		grp = h5.DataGroup('my_data', dat, description='pretty wise',
+			greets_from='WakA') # arbitrary metadata as kw
 
 		qt.mstart()
+		
+		cnt=0
 		for i in self.xs:
+			cnt+=1
 			data.add_coordinate('{%s} ({%s})' % (i.label,i.unit),
 				size=abs((i.end - i.begin) / i.stepsize),
 				start=i.begin,
 				end=i.end
 				)
+			grp.add_coordinate('X%d' % cnt, #keep track of the nth coordinate
+				label=i.label,
+				unit=i.unit,
+				size=abs((i.end - i.begin) / i.stepsize),
+				start=i.begin,
+				end=i.end
+				)
+		cnt=0
 		for i in self.zs:
+			cnt+=1
 			data.add_value('{%s} ({%s})' % (i.label,i.unit))
-		
+			grp.add_value('Z%d' % cnt, label=i.label,unit=i.unit)
+			
 		data.create_file()
         
 		plotvaldim =1
 		if len(self.xs) > 1:
-			plotvaldim = 2
-			plot3d = qt.Plot3D(data, name='measure3D', coorddims=(0,1), valdim=2, style='image')
-		plot2d = qt.Plot2D(data, name='measure2D', coorddim=1, valdim=plotvaldim, traceofs=10)
+			plotvaldim = len(self.xs)
+			plot3d = qt.Plot3D(data, name='measure3D', coorddims=(plotvaldim-2,plotvaldim-3), valdim=plotvaldim, style='image')
+		plot2d = qt.Plot2D(data, name='measure2D', coorddim=plotvaldim-1, valdim=plotvaldim, traceofs=10)
 		cnt = 0
-		for dp in self.traverse_gen(self.xs):
-			#datapoint extraction
-			print dp
-			i = dp['dp']
-			try:
-				for x in range(len(self.xs)):
-					self.xs[x].module(i[x])
-			except Exception as e:
-				print 'Exception caught while trying to set axes: ', e
-				
-			#t = lambda x,y: sin(x*2*pi)+sin(y*2*pi)
-			# if cnt == 0:
-				# qt.msleep(4) #wait 4 seconds to start measuring to allow for capacitive effects to dissipate
-				# cnt +=1
-			r = self.zs[0].module()
+		
+		try:
+			for dp in self.traverse_gen(self.xs):
+				#datapoint extraction
+# 				print dp
+				i = dp['dp']
 
-			allmeas = np.hstack((i,r))
-			print allmeas
-			data.add_data_point(*allmeas)
-			#read out the control bit if it exists..
-			#only used for communicating to plot3d and gnuplot to start a new datablock
-			try:
-				#fixme
-				if 'newblock' in dp and dp['newblock'] == 1:
-					data.new_block()
-			except Exception as e:
-				print e
-			qt.msleep(0.001)
-			
-		data.new_block()
+				try:
+					for x in range(len(self.xs)):
+						functocall = getattr(instruments[x],'set_%s' % self.xs[x].module_options['var'])
+						functocall(i[x])
+				except Exception as e:
+					print 'Exception caught while trying to set axes: ', e
+					
+				# if cnt == 0:
+					# qt.msleep(4) #wait 4 seconds to start measuring to allow for capacitive effects to dissipate
+					# cnt +=1
+				r = self.zs[0].module()
+	
+				allmeas = np.hstack((i,r))
+				print allmeas
+				
+				#get keys for all dimensions
+				kz = grp.group.keys()
+				#write to them
+				##todo: check if dataset exceeded
+				##expand dataset every time with 100?
+				for i in range(len(kz)):
+					grp[kz[i]] = np.append(grp[kz[i]], allmeas[i])
+				
+				data.add_data_point(*allmeas)
+				#read out the control bit if it exists..
+				#only used for communicating to plot3d and gnuplot to start a new datablock
+				try:
+					#fixme
+					if 'newblock' in dp and dp['newblock'] == 1:
+						data.new_block()
+				except Exception as e:
+					print e
+				
+				qt.msleep(0.001)
+		except Exception as e:
+			print 'excepted error:', e 
+			#handle an abort with some grace for hdf5 et.al.
+			dat.close()
+			from lib.file_support.spyview import SpyView
+			SpyView(data).write_meta_file()
+			print 'Wrapped up the datafiles'
+			raise
+
+
 		data.close_file()
+		dat.close()
 		from lib.file_support.spyview import SpyView
 		SpyView(data).write_meta_file()
 		
 		qt.mend()
-		print 'measurement ended'	
+		print 'measurement ended'
